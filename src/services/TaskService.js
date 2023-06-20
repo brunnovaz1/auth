01d16e5c2b93763e768ed1713060a524a81bbbd3
brunnovaz1 <brunnovaz@gmail.com>
2023-06-20 20:24:45 -0300

    arquivoFinal

--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,62 +1,53 @@
-
-import {urlApi} from "./FirebaseConfig"
-
-
-
+import { urlApi } from "./FirebaseConfig";
 
 //GET para listar
 
-export async function listaTarefas(){
-    let tarefas =[]                       
-    await  fetch(urlApi + "tarefas.json")               
-    .then((response)=> response.json())
-    .then((data) => {      
-        for(let key in data){      
-            tarefas.push({key, ...data[key]})
-        }
+export async function listaTarefas() {
+  let tarefas = [];
+  await fetch(urlApi + "tarefas.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let key in data) {
+        tarefas.push({ key, ...data[key] });
+      }
     })
-    .catch((error) => {throw Error("Deu ruim")})
-    return tarefas
+    .catch((error) => {
+      throw Error("Deu ruim");
+    });
+  return tarefas;
 }
-
-
-
-
 
 //POST  criar, inserir
 
-export async function insereTarefa(tarefa){
-   await fetch(urlApi + "tarefas.json",{
-        method: "POST",
-        body: JSON.stringify(tarefa),
-        headers:{
-            'Content-type': 'application/json'}
-        
-    })
-    .catch((error) => {throw Error("Deu ruim")})
+export async function insereTarefa(tarefa) {
+  await fetch(urlApi + "tarefas.json", {
+    method: "POST",
+    body: JSON.stringify(tarefa),
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).catch((error) => {
+    throw Error("Deu ruim");
+  });
 }
 
 //PUT alterar, modificar
-export async function modificaTarefa(tarefa){
-    await fetch(urlApi + "tarefas/" + tarefa.key + ".json",{
-         method: 'PUT',
-         body: JSON.stringify({nome: tarefa.nome, prioridade: tarefa.prioridade}),
-         headers:{'Content-type': 'application/json'}    
-     })
-     .catch((error) => {throw Error("Deu ruim")})
- }
- 
-
-
-
-
+export async function modificaTarefa(tarefa) {
+  await fetch(urlApi + "tarefas/" + tarefa.key + ".json", {
+    method: "PUT",
+    body: JSON.stringify({ nome: tarefa.nome, prioridade: tarefa.prioridade }),
+    headers: { "Content-type": "application/json" },
+  }).catch((error) => {
+    throw Error("Deu ruim");
+  });
+}
 
 //DELETE destruir, remover
 
-export async function removeTarefa(key){
-    await fetch(urlApi+ "tarefas/" + key + ".json", {
-        method:'DELETE'
-    })
-
-.catch((error) => {throw Error("Deu ruim")})
+export async function removeTarefa(key) {
+  await fetch(urlApi + "tarefas/" + key + ".json", {
+    method: "DELETE",
+  }).catch((error) => {
+    throw Error("Deu ruim");
+  });
 }
